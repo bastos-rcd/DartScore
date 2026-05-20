202928a26ec02be7cdfd3b5e699114e18351081f
Bastos <record.bastien@gmail.com>
2026-05-20 19:56:01 +0200

    feat: routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Menu from '@/components/menu'
+import Home from './pages/home'
+
 export default function App() {
-	return <>OK</>
+	return (
+		<Router>
+			<div className="flex h-screen max-h-screen flex-col-reverse gap-4 overflow-hidden p-4">
+				<Menu />
+
+				<hr className="h-0.5 border-0 bg-(--border)" />
+
+				<main className="flex-1">
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</main>
+			</div>
+		</Router>
+	)
 }
