8c27f98f1bf3c3cda8fb95d6f97bd5c53402ae5e
Bastos <record.bastien@gmail.com>
2026-05-21 13:17:42 +0200

    feat: game target adding score

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import type { Player } from '@/models/player'
 import { gameStore } from '@/store/game'
 
 import GameTarget from '@/components/game/game-target'
@@ -8,18 +9,41 @@ import GameTarget from '@/components/game/game-target'
 export default function Game() {
 	const navigate = useNavigate()
 
-	const { status } = gameStore()
+	const { status, players, darts } = gameStore()
+
+	const [current, setCurrent] = useState<Player>()
 
 	useEffect(() => {
 		if (!status) {
 			navigate('/')
 			return
 		}
-	}, [status])
+
+		const last = darts[darts.length - 1]
+
+		if (!last) {
+			setCurrent(players[0])
+			return
+		}
+
+		const thrown = darts.filter((d) => d.playerId === last.playerId).length
+		if (thrown % 3 === 0) {
+			const currentIndex = players.findIndex((p) => p.id === last.playerId)
+
+			if (currentIndex === players.length - 1) {
+				setCurrent(players[0])
+				return
+			}
+
+			setCurrent(players[currentIndex + 1])
+			return
+		}
+		setCurrent(players.find((p) => p.id === last.playerId))
+	}, [status, darts])
 
 	return (
 		<>
-			<GameTarget playerId="" />
+			<GameTarget playerId={current?.id || ''} />
 		</>
 	)
 }
