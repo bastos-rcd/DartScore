e7b412d62f3cad14ca532cb5638c6c581f111175
Bastos <record.bastien@gmail.com>
2026-05-22 12:03:40 +0200

    fix: rename route rank

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -13,7 +13,7 @@ export default function Menu() {
 		<nav className="flex flex-row justify-evenly">
 			<Item href="/" icon="fa-solid fa-bullseye fa-xl" />
 			<Item href="/players" icon="fa-solid fa-people-group fa-xl" />
-			<Item href="/statistics" icon="fa-solid fa-chart-simple fa-xl" />
+			<Item href="/rank" icon="fa-solid fa-chart-simple fa-xl" />
 			<Item href="/history" icon="fa-solid fa-clock-rotate-left fa-xl" />
 		</nav>
 	)
