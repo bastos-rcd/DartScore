72d7af29efa5a2ad3f73aeb22c59ec9f3508b2c6
Bastien Record <bastienr@mobix.fr>
2026-06-29 16:11:02 +0200

    fix: DevOps with Docker

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
+	server: {
+		host: true,
+		allowedHosts: ['*'],
+		hmr: {
+			clientPort: 443,
+			protocol: 'wss',
+		},
+	},
 })
