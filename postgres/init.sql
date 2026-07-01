1be27f1334d71bf795b95ebe1d402419bf3273b8
Bastien Record <bastienr@mobix.fr>
2026-07-01 15:55:44 +0200

    feat: db with user

---
+++ postgres/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "user"
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(5) DEFAULT 'USER' CHECK (role IN ('ADMIN', 'USER')),
+    active BOOLEAN DEFAULT TRUE
+);
+
+-- password : hashedpwd
+INSERT INTO "user" (name, email, password, role)
+VALUES ('ADMIN', 'admin@dartscore.fr', '$2b$10$GwKnopMR9OUARx1AzEhfFuwWw629JxMhpG2nyMUqYIW9zpERHO1Tq', 'ADMIN'); 
